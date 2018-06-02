12ea4516774a807fe2bc484bb0d5f60c80e1cc1f
Nadim Uddin <nadim.uddin1995@gmail.com>
2018-06-02 02:01:16 -0400

    added extra test for candidates

--- test/election.js
+++ test/election.js
@@ -1,12 +1,31 @@
 var Elections = artifacts.require("./Election.sol");
 
 contract("Election", (accounts)=>{
+    var electionInstance;
     it("initializes with two candiates", ()=>{
         return Elections.deployed().then((instance)=>{
             return instance.candidatesCount();
         })
         .then((count)=>{ 
             assert.equal(count, 2);
+        })
+    })
+
+    it("initializes the candidates with the correct value", ()=>{
+        return Elections.deployed().then((instance)=>{
+            electionInstance = instance
+            return instance.candidates(1)
+        })
+        .then(candidate => {
+            assert.equal(candidate[0], 1, "contains the correct id");
+            assert.equal(candidate[1], "Candidate 1", "contains the right name");
+            assert.equal(candidate[2], 0, "contains the right number of votes");
+            return electionInstance.candidates(2);
+        })
+        .then(candidate=> {
+            assert.equal(candidate[0], 2, "contains the correct id");
+            assert.equal(candidate[1], "Candidate 2", "contains the right name");
+            assert.equal(candidate[2], 0, "contains the right number of votes");
         })
     })
 })
