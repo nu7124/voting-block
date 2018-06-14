141c95a0dd0f8eb3edb8b38712e5fc196fb730bd
nu7124 <nadim.uddin1995@gmail.com>
2018-06-14 16:45:05 -0400

    passed in default parameters

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,6 +2,5 @@ var Election = artifacts.require("./Election.sol");
 var ElectionFactory = artifacts.require("./ElectionFactory.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(Election, creator="", allCandidates="", question="")
-    deployer.deploy(ElectionFactory)
+    deployer.deploy(Election, creator="", allCandidates=[], question="")
 };
