1917f570480cb5b823c20d6b908036426301c63a
nu7124 <nadim.uddin1995@gmail.com>
2018-06-14 21:55:29 -0400

    going back to solidity factory

--- contracts/ElectionFactory.sol
+++ contracts/ElectionFactory.sol
@@ -1,11 +1,31 @@
-// pragma solidity ^0.4.2;
+pragma solidity ^0.4.2;
 
-// import './Election.sol';
+import './Election.sol';
 
-// contract ElectionFactory {
+contract ElectionFactory {
 
-//     function newElection (address _creator, string allCandidates, string _question) public returns (address){
-//         Election e = (new Election(_creator, allCandidates, _question));
-//         return address(e);
-//     }
-// }
+    address[] allElections;
+
+    event print(string data);
+
+    function newElection (address _creator, string allCandidates, string _question) public returns (address){
+        print("ABOUT TO MAKE ELECTION");
+        address e = (new Election(_creator, allCandidates, _question));
+        print("MADE NEWELECTION");
+        allElections.push(e);
+        return e;
+    }
+
+    function plusCandidate(address elec, string candidate) public {
+        print("ABOUT TO MAKE ELECTION");
+        Election poll = Election(elec);
+        print("MADE ELECTION");
+        print("ABOUT TO MAKE POLL");
+        poll.addCandidate(candidate);
+    }
+
+    function getElection() public returns (Election){
+        print("ABOUT TO RETRIEVE ELECTION");
+        return Election(allElections[0]);
+    }
+}
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,5 +2,6 @@ var Election = artifacts.require("./Election.sol");
 var ElectionFactory = artifacts.require("./ElectionFactory.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(Election, creator="", allCandidates=[], question="")
+    deployer.deploy(Election, creator="", allCandidates=[], question="");
+    deployer.deploy(ElectionFactory);
 };
