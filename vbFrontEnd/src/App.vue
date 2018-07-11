0d9147748bf22e507c11cb4625b8f5d398171b37
nu7124 <nadim.uddin1995@gmail.com>
2018-07-11 13:04:18 -0400

    added vue front end folder, will soon purge the old src folder

---
+++ vbFrontEnd/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    background-color: #2C3443;
+    display: grid;
+    width:100vw;
+    height:100vh;
+}
+</style>
---
+++ vbFrontEnd/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div id="main_nav">
+        <h1 id="main_nav_title">Voting Block</h1>
+    </div>
+
+    <div id="main_myPolls">
+        <h2 class="main_myPolls_title">MY POLLS</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#main_nav {
+    background-color: #444A56;
+    width:100vw;
+    height:10vh;
+    display: grid;
+    justify-content: center;
+    align-content: center;
+}
+
+#main_nav_title {
+    color:#ffefdf;
+    font-family: 'Kaushan Script', cursive;
+}
+
+#main_myPolls {
+    padding:2%;
+}
+
+.main_myPolls_title {
+    color:#ffefdf;
+    font-family: 'Indie Flower', cursive;
+}
+</style>
